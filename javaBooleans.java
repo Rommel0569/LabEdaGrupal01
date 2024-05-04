803fbcf8e5b3895ebb4bced2b8be45fb6efcb536
Rommel0569 <rchambive@unsa.edu.pe>
2024-05-04 18:40:57 -0500

    13ava clase de java tutorial

---
+++ javaBooleans.java
@@ -0,0 +1,29 @@
+public class javaBooleans {
+  public static void main(String[] args) {
+    
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);     // Outputs true
+System.out.println(isFishTasty);   // Outputs false
+
+/////////example
+int x = 10;
+int y = 9;
+System.out.println(x > y); // returns true, because 10 is higher than 9
+
+System.out.println(10 > 9); // returns true, because 10 is higher than 9
+  int myAge = 25;
+int votingAge = 18;
+System.out.println(myAge >= votingAge);
+  ////example fin
+  int myAge = 25;
+int votingAge = 18;
+
+if (myAge >= votingAge) {
+  System.out.println("Old enough to vote!");
+} else {
+  System.out.println("Not old enough to vote.");
+}
+  
+  }
+}
