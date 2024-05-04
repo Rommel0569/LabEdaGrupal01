b5e11d5d92bab9e4de31f8e383c6b3ba7b641df4
Rommel0569 <rchambive@unsa.edu.pe>
2024-05-04 18:12:05 -0500

    Confirm

---
+++ typesdata.java
@@ -0,0 +1,35 @@
+public class typesdata{
+    public static void main(String []args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+    ////////numeros
+    byte myNum = 100;
+    System.out.println(myNum);
+
+    float f1 = 35e3f;
+    double d1 = 12E4d;
+    System.out.println(f1);
+    System.out.println(d1);
+
+    /////boleans
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;
+    System.out.println(isJavaFun);     // Outputs true
+    System.out.println(isFishTasty);   // Outputs false
+
+
+    /////character
+    char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+    System.out.println(myVar1);
+    System.out.println(myVar2);
+    System.out.println(myVar3);
+
+    ////tipos no primitive
+    
+
+
+    }
+}
