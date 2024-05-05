c65b75ed76c93d3f4205bcb1f439dcd57621f4a2
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-05 15:33:57 -0500

    Se modifica y aumenta mas lineas de impresión

--- javaOutput.java
+++ javaOutput.java
@@ -3,5 +3,11 @@ public class javaOutput {
         System.out.println("Hola Mundo!"); //Imprime el mensaje por defecto
         System.out.println("Estoy aprendiendo Java");
         System.out.println("Es increíble!");
+
+        System.out.println(3); //Imprime números
+        System.out.println(358);
+        System.out.println(50000);
+
+        System.out.println(2 * 5); //Imprime un ejercicio
     }
 }
