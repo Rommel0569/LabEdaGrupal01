6950d757ef1f8db883ab520e028764e0ec5349a3
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-05 16:09:25 -0500

    16va clase de java tutorial

---
+++ javaWhileLoop.java
@@ -0,0 +1,17 @@
+public class javaWhileLoop {
+    public static void main (String[] args){
+        //Bucle While
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
+        //Bucle Do-While
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 5);
+    }
+}
