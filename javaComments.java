00d8757b03fd0ceb31161460f8f346dfe3d4c03b
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-05 16:07:11 -0500

    6ta clase de java tutorial

---
+++ javaComments.java
@@ -0,0 +1,12 @@
+public class javaComments {
+    public static void main (String[] args){
+        // This is a comment
+        System.out.println("Hola Mundo");
+
+        System.out.println("Hola Mundo"); // This is a comment
+
+        /* The code below will print the words Hello World
+        to the screen, and it is amazing */
+        System.out.println("Hola Mundo");
+    }
+}
