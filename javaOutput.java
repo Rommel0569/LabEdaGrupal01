bb5c268c928a65828eac9f4fddc08eadfa0eccaa
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-05-05 15:28:59 -0500

    2da clase java tutorial

---
+++ javaOutput.java
@@ -0,0 +1,7 @@
+public class javaOutput {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!"); //Imprime el mensaje por defecto
+        System.out.println("Estoy aprendiendo Java");
+        System.out.println("Es increíble!");
+    }
+}
